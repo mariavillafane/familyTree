08107ffb8ccd4420e0596991a4c48d895b628d57
Maria Villafane <eugeniavil@gmail.com>
2023-01-02 23:31:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Grid ({gridWidth, gridHeight, children}){
+  return(
+    <div style = {{
+      display: "grid",
+      gridGap: "10px",
+      gridTemplateColumns: `repeat(${gridWidth}, 100px)`,
+      gridTemplateRows: `repeat(${gridHeight}, 100px)`,
+      backgroundColor: "#afb",
+      color: "#444",
+      }}>
+       {children}
+    </div>
+  )
+}
+
+function coordinates(x,y){
+  return{
+    gridColumn: x,
+    gridRow: y,
+  }
+
+}
+
+function Box({x,y,T,D,R,L}){
+  return(
+      <div style = {{
+          ...coordinates(x,y),
+          backgroundColor: "#bff",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",     
+          gridTemplateRows: "1fr 1fr",      
+          borderRadius: "5px"    
+        }}>
+          <div style = {{
+          ...coordinates(1,1),          
+          borderBottom: L? "#aaa 3px solid": undefined, 
+          borderRight: T? "#aaa 3px solid": undefined, 
+          //borderRadius: "5px"
+          // backgroundColor: "#fff",          
+        }}></div>
+          <div style = {{
+          ...coordinates(2,2),
+          borderTop: R? "#aaa 3px solid": undefined, 
+          borderLeft: D? "#aaa 3px solid": undefined,           
+          //borderRadius: "5px"
+          // backgroundColor: "#fff",          
+        }}></div>
+
+      </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid gridWidth={6} gridHeight={4}>
+      <Box x={3} y={2} T R/>
+      <Box x={3} y={3} T R/>
+
+    </Grid>
   );
 }
 
